d5387810138ae826625938c4617e776ab90792cf
AlpacaMan009 <tahmid5678@gmail.com>
2026-03-27 21:01:20 +0600

    Display filtered cards function done

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
-document.getElementById("signIn-btn").addEventListener('click',function(){
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+document.getElementById("signIn-btn").addEventListener("click", function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    console.log(username)
-    console.log(password)
+  console.log(username);
+  console.log(password);
 
+  const defUsername = "admin";
+  const defPassword = "admin123";
 
-    const defUsername = 'admin';
-    const defPassword = 'admin123';
+  if (username === defUsername && password === defPassword) {
+    alert("Login Successfull!!");
+  } else {
+    alert("Login Failed");
+  }
+});
 
-    if(username === defUsername && password === defPassword){
-        alert("Login Successfull!!")
-    }
-
-    else{
-        alert("Login Failed")
-    }
-})
-
-
-// Spinner 
+// Spinner
 
 const manageSpinner = (status) => {
-    if(status==true){
-        document.getElementById("spinner").classList.remove("hidden")
-        document.getElementById("word-container").classList.add("hidden")
-    }
-    else{
-        document.getElementById("spinner").classList.add("hidden")
-        document.getElementById("word-container").classList.remove("hidden")
-    }
-}
-
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("word-container").classList.remove("hidden");
+  }
+};
 
 // const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 // fetch(url)
 // .then(res => res.json())
 // .then(data => console.log(data.data))
 
-
-
 // Remoev active class from all buttons
 
-const removeActive = () =>{
-    const filterBtns = document.querySelectorAll(".filter-btn");
-    console.log("found filter btns:" , filterBtns)
-    filterBtns.forEach(btn => btn.classList.remove("active"));
-}
+const removeActive = () => {
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  console.log("found filter btns:", filterBtns);
+  filterBtns.forEach((btn) => btn.classList.remove("active"));
+};
 
-const activeBtn = (btnId) =>{
-    const button = document.getElementById(btnId);
-    button.classList.add("active");
-}
-
-
+const activeBtn = (btnId) => {
+  const button = document.getElementById(btnId);
+  button.classList.add("active");
+};
 
 
 
+
+
+// Displaying Filtered Cards
+const filteredCardsDisplay = (filter) => {
+  let filterCards = "";
+
+  if (filter === "all") {
+    filterCards == cardsData;
+  } 
+  
+  else if (filter === "opened") {
+    filterCard = cardData.filter((card) => card.status === "opened");
+  }
+
+   else if (filter === "closed") {
+    filterCard = cardData.filter((card) => card.status === "closed");
+  }
+
+
+
+
+};
