fbf6c488d4750a176520ec0c172fff2207f4f4e3
AlpacaMan009 <tahmid5678@gmail.com>
2026-04-12 22:07:47 +0600

    renamed the signin.html to index.html and index.html to dashboard.html

--- signin.js
+++ signin.js
@@ -9,9 +9,9 @@ document.getElementById("signIn-btn").addEventListener("click", function () {
   const defPassword = "admin123";
 
   if (username === defUsername && password === defPassword) {
-    // alert("Sign-in Successfull!!");
+    alert("Sign-in Successfull!!");
     localStorage.setItem("loggedIn", "true");
-    window.location.assign("index.html")
+    window.location.assign("dashboard.html")
   } else {
     alert("Sign-in Failed");
   }
